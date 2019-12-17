93e35f1cdef81adcf76b28c8ea38e50c73542cae
pronia00 <pronia00@ya.ru>
2019-12-17 13:07:03 +0300

    [In progress] + Database::Put() - tested + Database::GetByID() - tested

--- BrownBelt/Week_1/SecondaryIndex/SecondaryIndex.cpp
+++ BrownBelt/Week_1/SecondaryIndex/SecondaryIndex.cpp
@@ -4,7 +4,12 @@
 #include <map>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
+#include <list>
+#include <sstream>
+#include <iterator>
+
 
 using namespace std;
 
@@ -22,7 +27,9 @@ struct Record {
       , timestamp(_ts)
       , karma(_karma)
   {};
-
+  void operator<<(ostream& os) {
+    os << id << title << user << timestamp << karma;
+  }
   bool operator==(const Record& other) const {
     return 
       make_tuple(id, title, user, timestamp, karma) ==
@@ -36,21 +43,52 @@ struct HashRecord {
   const size_t coef = 2'953'737;
 
   size_t operator() (const Record& rec) const {
-    return ( 
-        coef * coef * coef * coef * string_hash(rec.id) +
-               coef * coef * coef * string_hash(rec.title) +
-                      coef * coef * string_hash(rec.user) +
-                             coef * int_hash(rec.timestamp) +
-                                    int_hash(rec.karma)
-    );
+    // return ( 
+    //     coef * coef * coef * coef * string_hash(rec.id) +
+    //            coef * coef * coef * string_hash(rec.title) +
+    //                   coef * coef * string_hash(rec.user) +
+    //                          coef * int_hash(rec.timestamp) +
+    //                                 int_hash(rec.karma)
+    // );
+    return string_hash(rec.id);
   }
 };
 
+struct SecondaryIndex {
+    list<Record>::iterator list_iter;
+
+};
 // Реализуйте этот класс
 class Database {
 public:
-  bool Put(const Record& record);
-  const Record* GetById(const string& id) const;
+  bool Put(const Record& record) {
+    if (id_data.find(record.id) != id_data.end()) {
+        return false;
+    }
+    //insert data to main storage 
+    data.push_front(record);
+
+    /*insert data to secondary storages
+        ...
+    */
+
+    //create seondary index
+    SecondaryIndex index {
+      data.begin()
+    };
+
+    //move secondary index to hash_map for O(1) access by index
+    id_data[record.id] = move(index);
+    return true;
+  }
+
+  const Record* GetById(const string& id) const {
+    auto res = id_data.find(id);
+    return (res == id_data.end())
+      ? nullptr
+      : &(*(res->second.list_iter));
+
+  }
   bool Erase(const string& id);
 
   template <typename Callback>
@@ -61,7 +99,41 @@ public:
 
   template <typename Callback>
   void AllByUser(const string& user, Callback callback) const;
+
+private: 
+  list<Record> data;
+  unordered_map<string, SecondaryIndex, hash<string>> id_data;
 };
+
+void TestPut() {
+  Database db;
+  Record r1{"1", "title", "user", 1, 1};
+  Record r2{"2", "title", "user", 1, 1};
+
+  bool result = db.Put(r1);
+  ASSERT(result);
+
+  result = db.Put(r2);
+  ASSERT(result);
+
+  result = db.Put(r1);
+  ASSERT(!result);
+}
+
+void TestGetById() {
+  Database db;
+  Record r1{"1", "title", "user", 1, 1};
+  Record r2{"2", "title", "user", 1, 1};
+  Record r3{"3", "title", "user", 1, 1};
+
+  db.Put(r1);
+  db.Put(r2);
+  db.Put(r3);
+  
+  ASSERT(r1 == *(db.GetById("1")));
+  ASSERT(r2 == *(db.GetById("2")));
+  ASSERT(!(r1 == *(db.GetById("2"))));
+}
 
 void TestRecordEquality() {
   Record r1("one", "two", "tree", 1, 2);
@@ -122,6 +194,8 @@ void TestRecordEquality() {
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestRecordEquality);
+  RUN_TEST(tr, TestPut);
+  RUN_TEST(tr, TestGetById);
   //RUN_TEST(tr, TestRangeBoundaries);
   //RUN_TEST(tr, TestSameUser);
   //RUN_TEST(tr, TestReplacement);
