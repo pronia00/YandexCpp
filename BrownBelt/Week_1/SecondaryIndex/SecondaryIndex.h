eaad4885eb6ade1b6782e613510ec5b109e6e672
pronia00 <pronia00@ya.ru>
2019-12-18 13:38:44 +0300

    [Done | Refactoring] + struct RangeBy for refactoring RangeBy... functions

--- BrownBelt/Week_1/SecondaryIndex/SecondaryIndex.h
+++ BrownBelt/Week_1/SecondaryIndex/SecondaryIndex.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <list>
+#include <iterator>
 
 using namespace std;
 
@@ -42,7 +43,6 @@ struct HashRecord {
 // Реализуйте этот класс
 class Database {
 public:
-    using data_iter = list<Record>::iterator;
 
     // insert elements if <id> does not exist
     bool Put(const Record& record);
@@ -53,46 +53,56 @@ public:
     // erase if <id> does not exist
     bool Erase(const string& id);
 
-    // count elements in range [low, high]
-    template <typename Callback, typename CheckMax>
-    void RangeByTimestamp(int low, int high, Callback callback) const {
-        auto range_begin = timestamp_data.lower_bound(low);
-        auto range_end = timestamp_data.end();
+    template <typename Container, typename Data>
+    class RangeBy {
+       public: 
+        using const_iter = typename Container::const_iterator;
+        RangeBy(const Container& cont, const Data& low) 
+          : _begin(cont.lower_bound(low))
+          , _end(cont.end())
+        {}
+        const_iter begin() const { return _begin; }
+        const_iter end() const   { return _end; }
+      private:
+        const_iter _begin;
+        const_iter _end;
+    };
 
-        for (auto cur = range_begin; cur != range_end; cur++) {
-            if (cur->second->timestamp > high) break;
-            if (!callback(*(cur->second))) break;
+    // count elements in range [low, high]
+    template <typename Callback>
+    void RangeByTimestamp(int low, int high, Callback callback) const {
+        for (auto& cur : RangeBy<i_SI, int>(timestamp_data, low)) {
+            if (cur.second->timestamp > high) break;
+            if (!callback(cur.second)) break;
         }
     }
     // count elements in range [low, high]
     template <typename Callback>
     void RangeByKarma(int low, int high, Callback callback) const {
-        auto range_begin = karma_data.lower_bound(low);
-        auto range_end = karma_data.end();
-
-        for (auto cur = range_begin; cur != range_end; ++cur) {
-            if (cur->second->karma > high) break;
-            if (!callback(*(cur->second))) break;
+          for (auto& cur : RangeBy<i_SI, int>(karma_data, low)) {
+            if (cur.second->karma > high) break;
+            if (!callback(*(cur.second))) break;
         }
     }
     // count elements with equal <user>
     template <typename Callback>
     void AllByUser(const string& user, Callback callback) const {
-        auto range_begin = user_data.lower_bound(user);
-        auto range_end = user_data.end();
-        
-        for (auto cur = range_begin; cur != range_end; ++cur) {
-            if (cur->second->user > user)  break;
-            if (!callback(*(cur->second))) break;
+        for (auto& cur : RangeBy<s_SI, string>(user_data, user)) {
+            if (cur.second->user > user)  break;
+            if (!callback(*(cur.second))) break;
         }
     }
 
 private: 
+    using data_iter = typename list<Record>::iterator;
+    using i_SI = multimap<int, data_iter>;
+    using s_SI = multimap<string, data_iter>;
+
     struct SecondaryIndexElement {
       data_iter list_iter;
-      multimap<int, data_iter>::iterator ts_iter; 
-      multimap<int, data_iter>::iterator karma_iter;
-      multimap<string, data_iter>::iterator user_iter;
+      i_SI::iterator ts_iter; 
+      i_SI::iterator karma_iter;
+      s_SI::iterator user_iter;
     };
     
     //main data storage 
@@ -100,9 +110,9 @@ private:
 
     // O(1) access to elements by index
     unordered_map<string, SecondaryIndexElement, hash<string>> id_data;
-    
+
     //Secondary Index Containers
-    multimap<int, data_iter> timestamp_data;
-    multimap<int, data_iter> karma_data;
-    multimap<string, data_iter> user_data;
+    i_SI timestamp_data;
+    i_SI karma_data;
+    s_SI user_data;
 };
