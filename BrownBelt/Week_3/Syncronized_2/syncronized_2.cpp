107d79bdfdaa2383ae1ce52d3b596618dc6d8cb8
pronia00 <pronia00@ya.ru>
2020-01-13 14:20:38 +0300

    [Done] Syncronized_2

--- BrownBelt/Week_3/Syncronized_2/syncronized_2.cpp
+++ BrownBelt/Week_3/Syncronized_2/syncronized_2.cpp
@@ -9,15 +9,52 @@
 using namespace std;
 
 template <typename T>
-class Synchronized {
+class Synchronized2 {
 public:
-  explicit Synchronized(T initial = T());
+  explicit Synchronized2(T initial = T())
+    : value(move(initial))
+  {
+  }
 
-  ??? GetAccess();
-  ??? GetAccess() const;
+  struct Access {
+    T& ref_to_value;
+    lock_guard<mutex> guard;
+  };
+
+  Access GetAccess() {
+    return {value, lock_guard(m)};
+  }
 
 private:
   T value;
+  mutex m;
+};
+
+template <typename T>
+class Synchronized {
+public:
+  explicit Synchronized(T initial = T())
+    : value(move(initial))
+  {};
+
+
+  template <typename U>
+  struct Access {
+    U& ref_to_value;
+    lock_guard<mutex> lock;
+  };
+
+  Access<T> GetAccess() {
+    return Access<T>{value, lock_guard(m)};
+  }
+  
+  Access<const T> GetAccess() const {
+    return Access<const T>{value, lock_guard(m)};
+  }
+
+private:
+  T value;
+  mutable mutex m;
 };
 
 void TestConcurrentUpdate() {
@@ -47,15 +84,7 @@ vector<int> Consume(Synchronized<deque<int>>& common_queue) {
     deque<int> q;
 
     {
-      // ÐœÑ‹ ÑÐ¿ÐµÑ†Ð¸Ð°Ð»ÑŒÐ½Ð¾ Ð·Ð°ÐºÐ»ÑŽÑ‡Ð¸Ð»Ð¸ ÑÑ‚Ð¸ Ð´Ð²Ðµ ÑÑ‚Ñ€Ð¾Ñ‡ÐºÐ¸ Ð² Ð¾Ð¿ÐµÑ€Ð°Ñ‚Ð¾Ñ€Ð½Ñ‹Ðµ ÑÐºÐ¾Ð±ÐºÐ¸, Ñ‡Ñ‚Ð¾Ð±Ñ‹
-      // ÑƒÐ¼ÐµÐ½ÑŒÑˆÐ¸Ñ‚ÑŒ Ñ€Ð°Ð·Ð¼ÐµÑ€ ÐºÑ€Ð¸Ñ‚Ð¸Ñ‡ÐµÑÐºÐ¾Ð¹ ÑÐµÐºÑ†Ð¸Ð¸. ÐŸÐ¾Ñ‚Ð¾Ðº-Ð¿Ð¾Ñ‚Ñ€ÐµÐ±Ð¸Ñ‚ÐµÐ»ÑŒ Ð·Ð°Ñ…Ð²Ð°Ñ‚Ñ‹Ð²Ð°ÐµÑ‚
-      // Ð¼ÑŒÑŽÑ‚ÐµÐºÑ, Ð¿ÐµÑ€ÐµÐ¼ÐµÑ‰Ð°ÐµÑ‚ Ð²ÑÑ‘ ÑÐ¾Ð´ÐµÑ€Ð¶Ð¸Ð¼Ð¾Ðµ Ð¾Ð±Ñ‰ÐµÐ¹ Ð¾Ñ‡ÐµÑ€ÐµÐ´Ð¸ Ð² ÑÐ²Ð¾ÑŽ
-      // Ð»Ð¾ÐºÐ°Ð»ÑŒÐ½ÑƒÑŽ Ð¿ÐµÑ€ÐµÐ¼ÐµÐ½Ð½ÑƒÑŽ Ð¸ Ð¾Ñ‚Ð¿ÑƒÑÐºÐ°ÐµÑ‚ Ð¼ÑŒÑŽÑ‚ÐµÐºÑ. ÐŸÐ¾ÑÐ»Ðµ ÑÑ‚Ð¾Ð³Ð¾ Ð¾Ð½ Ð¾Ð±Ñ€Ð°Ð±Ð°Ñ‚Ñ‹Ð²Ð°ÐµÑ‚
-      // Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹ Ð² Ð¾Ñ‡ÐµÑ€ÐµÐ´Ð¸ Ð·Ð° Ð¿Ñ€ÐµÐ´ÐµÐ»Ð°Ð¼Ð¸ ÐºÑ€Ð¸Ñ‚Ð¸Ñ‡ÐµÑÐºÐ¾Ð¹ ÑÐµÐºÑ†Ð¸Ð¸, Ð¿Ð¾Ð·Ð²Ð¾Ð»ÑÑ
-      // Ð¿Ð¾Ñ‚Ð¾ÐºÑƒ-Ð¿Ñ€Ð¾Ð¸Ð·Ð²Ð¾Ð´Ð¸Ñ‚ÐµÐ»ÑŽ Ð¿Ð°Ñ€Ð°Ð»Ð»ÐµÐ»ÑŒÐ½Ð¾ Ð¿Ð¾Ð¼ÐµÑ‰Ð°Ñ‚ÑŒ Ð² Ð¾Ñ‡ÐµÑ€ÐµÐ´ÑŒ Ð½Ð¾Ð²Ñ‹Ðµ Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹.
-      //
-      // Ð Ð°Ð·Ð¼ÐµÑ€ ÐºÑ€Ð¸Ñ‚Ð¸Ñ‡ÐµÑÐºÐ¾Ð¹ ÑÐµÐºÑ†Ð¸Ð¸ ÑÑƒÑ‰ÐµÑÑ‚Ð²ÐµÐ½Ð½Ð¾ Ð²Ð»Ð¸ÑÐµÑ‚ Ð½Ð° Ð±Ñ‹ÑÑ‚Ñ€Ð¾Ð´ÐµÐ¹ÑÑ‚Ð²Ð¸Ðµ
-      // Ð¼Ð½Ð¾Ð³Ð¾Ð¿Ð¾Ñ‚Ð¾Ñ‡Ð½Ñ‹Ñ… Ð¿Ñ€Ð¾Ð³Ñ€Ð°Ð¼Ð¼.
+        
       auto access = common_queue.GetAccess();
       q = move(access.ref_to_value);
     }
