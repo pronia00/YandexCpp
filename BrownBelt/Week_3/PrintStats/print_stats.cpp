edf5b41f205f944b37ede7c3140f3ea003f9ead4
pronia00 <pronia00@ya.ru>
2020-01-13 12:03:43 +0300

    [Done] - just copied the solution

--- BrownBelt/Week_3/PrintStats/print_stats.cpp
+++ BrownBelt/Week_3/PrintStats/print_stats.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <optional>
 #include <numeric>
 
 using namespace std;
@@ -27,11 +28,6 @@ private:
   Iterator first, last;
 };
 
-template <typename Collection>
-auto Head(Collection& v, size_t top) {
-  return IteratorRange{v.begin(), next(v.begin(), min(top, v.size()))};
-}
-
 struct Person {
   string name;
   int age, income;
@@ -52,12 +48,92 @@ vector<Person> ReadPeople(istream& input) {
   return result;
 }
 
-int main() {
-  vector<Person> people = ReadPeople(cin);
+template <typename Iter>
+std::optional<string> FindMostPopularName(IteratorRange<Iter> range) {
+  if (range.begin() == range.end()) {
+    return std::nullopt;
+  } else {
+    sort(range.begin(), range.end(), [](const Person& lhs, const Person& rhs) {
+      return lhs.name < rhs.name;
+    });
+    const string* most_popular_name = &range.begin()->name;
+    int count = 1;
+    for (auto i = range.begin(); i != range.end(); ) {
+      auto same_name_end = find_if_not(i, range.end(), [i](const Person& p) {
+        return p.name == i->name;
+      });
+      const auto cur_name_count = std::distance(i, same_name_end);
+      if (
+        cur_name_count > count ||
+        (cur_name_count == count && i->name < *most_popular_name)
+      ) {
+        count = cur_name_count;
+        most_popular_name = &i->name;
+      }
+      i = same_name_end;
+    }
+    return *most_popular_name;
+  }
+}
+
+struct StatsData {
+  std::optional<string> most_popular_male_name;
+  std::optional<string> most_popular_female_name;
+  vector<int> cumulative_wealth;
+  vector<Person> sorted_by_age;
+};
+
+StatsData BuildStatsData(vector<Person> people) {
+  StatsData result;
+
+  {
+    IteratorRange males{
+      begin(people),
+      partition(begin(people), end(people), [](const Person& p) {
+        return p.is_male;
+      })
+    };
+    IteratorRange females{males.end(), end(people)};
+
+    // По мере обработки запросов список людей не меняется, так что мы можем
+    // один раз найти самые популярные женское и мужское имена
+    result.most_popular_male_name = FindMostPopularName(males);
+    result.most_popular_female_name = FindMostPopularName(females);
+  }
+
+  // Запросы WEALTHY можно тоже обрабатывать за О(1), один раз отсортировав всех
+  // людей по достатку и посчитав массив префиксных сумм
+  {
+    sort(people.begin(), people.end(), [](const Person& lhs, const Person& rhs) {
+      return lhs.income > rhs.income;
+    });
+
+    auto& wealth = result.cumulative_wealth;
+    wealth.resize(people.size());
+    if (!people.empty()) {
+      wealth[0] = people[0].income;
+      for (size_t i = 1; i < people.size(); ++i) {
+        wealth[i] = wealth[i - 1] + people[i].income;
+      }
+    }
+  }
 
   sort(begin(people), end(people), [](const Person& lhs, const Person& rhs) {
     return lhs.age < rhs.age;
   });
+  result.sorted_by_age = std::move(people);
+
+  return result;
+}
+
+int main() {
+  // Основной проблемой исходного решения было то, что в нём случайно изменялись
+  // входные данные. Чтобы ганатировать, что этого не произойдёт, мы организовываем код
+  // так, чтобы в месте обработки запросов были видны только константные данные.
+  //
+  // Для этого всю их предобработку мы вынесли в отдельную функцию, результат которой
+  // сохраняем в константной переменной.
+  const StatsData stats = BuildStatsData(ReadPeople(cin));
 
   for (string command; cin >> command; ) {
     if (command == "AGE") {
@@ -65,62 +141,31 @@ int main() {
       cin >> adult_age;
 
       auto adult_begin = lower_bound(
-        begin(people), end(people), adult_age, [](const Person& lhs, int age) {
+        begin(stats.sorted_by_age),
+        end(stats.sorted_by_age),
+        adult_age,
+        [](const Person& lhs, int age) {
           return lhs.age < age;
         }
       );
 
-      cout << "There are " << std::distance(adult_begin, end(people))
+      cout << "There are " << std::distance(adult_begin, end(stats.sorted_by_age))
            << " adult people for maturity age " << adult_age << '\n';
     } else if (command == "WEALTHY") {
       int count;
       cin >> count;
-
-      auto head = Head(people, count);
-
-      partial_sort(
-        head.begin(), head.end(), end(people), [](const Person& lhs, const Person& rhs) {
-          return lhs.income > rhs.income;
-        }
-      );
-
-      int total_income = accumulate(
-        head.begin(), head.end(), 0, [](int cur, Person& p) {
-          return p.income += cur;
-        }
-      );
-      cout << "Top-" << count << " people have total income " << total_income << '\n';
+      cout << "Top-" << count << " people have total income "
+           << stats.cumulative_wealth[count - 1] << '\n';
     } else if (command == "POPULAR_NAME") {
       char gender;
-      cin >> gender;
-
-      IteratorRange range{
-        begin(people),
-        partition(begin(people), end(people), [gender](Person& p) {
-          return p.is_male = (gender == 'M');
-        })
-      };
-      if (range.begin() == range.end()) {
-        cout << "No people of gender " << gender << '\n';
-      } else {
-        sort(range.begin(), range.end(), [](const Person& lhs, const Person& rhs) {
-          return lhs.name < rhs.name;
-        });
-        const string* most_popular_name = &range.begin()->name;
-        int count = 1;
-        for (auto i = range.begin(); i != range.end(); ) {
-          auto same_name_end = find_if_not(i, range.end(), [i](const Person& p) {
-            return p.name == i->name;
-          });
-          auto cur_name_count = std::distance(i, same_name_end);
-          if (cur_name_count > count) {
-            count = cur_name_count;
-            most_popular_name = &i->name;
-          }
-          i = same_name_end;
-        }
+      cin >>gender;
+      const auto& most_popular_name = gender == 'M' ? stats.most_popular_male_name
+                                                    : stats.most_popular_female_name;
+      if (most_popular_name) {
         cout << "Most popular name among people of gender " << gender << " is "
              << *most_popular_name << '\n';
+      } else {
+        cout << "No people of gender " << gender << '\n';
       }
     }
   }
